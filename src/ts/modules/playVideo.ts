18082030773c2f95a7fc9cac85caadba23195000
Ilya Belyakov <darksiders1282@gmail.com>
2024-03-19 20:42:11 +0300

    Update video playback functionality on second page

--- src/ts/modules/playVideo.ts
+++ src/ts/modules/playVideo.ts
@@ -6,10 +6,12 @@ export interface IVideoPlayer {
 }
 
 export class VideoPlayer {
-	buttons: NodeListOf<HTMLElement>;
+	buttons: NodeListOf<HTMLButtonElement>;
 	overlay: HTMLElement | null;
 	closeButton: HTMLElement | null;
 	player: YT.Player | null;
+	path: string | null;
+	activeButton: HTMLButtonElement | null;
 
 	constructor({ triggersSelector, overlaySelector }: IVideoPlayer) {
 		this.buttons = document.querySelectorAll(triggersSelector);
@@ -17,16 +19,33 @@ export class VideoPlayer {
 		if (!this.overlay) throw new Error('Overlay not found');
 		this.closeButton = this.overlay.querySelector('.video .close');
 		this.player = null;
+		this.path = null;
+		this.activeButton = null;
 	}
 
 	bindTriggers() {
-		this.buttons.forEach(button => {
+		this.buttons.forEach((button, i) => {
+			const blockedElement = button.closest('.module__video-item')?.nextElementSibling;
+			if (i % 2 === 0) blockedElement?.setAttribute('data-disabled', 'true');
+
 			button.addEventListener('click', () => {
+				const isDisabled =
+					button.closest('.module__video-item')?.getAttribute('data-disabled') === 'true';
+				if (isDisabled) return;
+
+				this.activeButton = button;
+				const newPath = button.getAttribute('data-url');
+				if (!newPath) return;
+
 				if (this.overlay && document.querySelector('iframe#frame')) {
 					this.overlay.style.display = 'flex';
+					if (this.path !== newPath) {
+						this.path = newPath;
+						this.player?.loadVideoById({ videoId: this.path });
+					}
 				} else {
-					const url = button.getAttribute('data-url');
-					if (url) this.createPlayer(url);
+					this.path = newPath;
+					this.createPlayer(this.path);
 				}
 			});
 		});
@@ -45,12 +64,34 @@ export class VideoPlayer {
 			height: '100%',
 			width: '100%',
 			videoId,
+			events: {
+				onStateChange: this.onPlayerStateChange,
+			},
 		});
 		if (this.overlay) {
 			this.overlay.style.display = 'flex';
 		}
 	}
 
+	onPlayerStateChange = (state: YT.OnStateChangeEvent) => {
+		const blockedElement =
+			this.activeButton?.closest('.module__video-item')?.nextElementSibling;
+		if (state.data === 0 && blockedElement && blockedElement instanceof HTMLElement) {
+			const playButton = this.activeButton?.querySelector('svg')?.cloneNode(true);
+			if (playButton) {
+				blockedElement.querySelector('svg')?.remove();
+				blockedElement.querySelector('.play__circle')?.appendChild(playButton);
+			}
+			const playText = blockedElement.querySelector('.play__text');
+			if (playText) {
+				playText.textContent = 'play video';
+				playText.classList.remove('attention');
+			}
+			blockedElement.style.opacity = '1';
+			blockedElement.style.filter = 'none';
+			blockedElement.setAttribute('data-disabled', 'false');
+		}
+	};
 	init() {
 		const scriptTag = document.createElement('script');
 		scriptTag.src = YOUTUBE_IFRAME_API_URL;
